409ffd713e7ad72e4bb5e45fd2356b05e3e3051f
ngigi1234 <francis.ngigi1@student.moringaschool.com>
2024-03-18 12:35:10 +0300

    added body content

---
+++ index.jsx
@@ -0,0 +1,2 @@
+<h1>Hello world</h1>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque aut quasi suscipit esse! Eveniet consequuntur odit, assumenda fugiat corporis sapiente porro facere dicta, eum id provident nihil laudantium qui error.
